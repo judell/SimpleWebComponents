b9a9b1836611de116e565209b19a0ada27550d57
Jon Udell <jon@jonudell.info>
2025-02-02 14:58:07 -0800

    add db init

---
+++ init.sql
@@ -0,0 +1,15 @@
+create table cities (
+    id integer primary key,
+    city text,
+    state text,
+    population integer
+);
+
+
+create table books (
+    id integer primary key,
+    title text,
+    author text,
+    year integer
+);
+
